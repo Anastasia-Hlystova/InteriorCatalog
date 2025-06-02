a400133670ae31583df8d4eb80787d6a840d7d3d
Anastasia-Hlystova <m2408317@edu.misis.ru>
2025-06-02 22:03:23 +0300

    Add files via upload

---
+++ Model/Core/Serialization/BaseSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Model.Core.Serialization
+{
+    public abstract class BaseSerializer<T>
+    {
+        public abstract string Serialize(T obj);
+        public abstract T Deserialize(string data);
+
+        public void SaveToFile(T obj, string filePath)
+        {
+            string serializedData = Serialize(obj);
+            File.WriteAllText(filePath, serializedData);
+        }
+
+        public T LoadFromFile(string filePath)
+        {
+            string data = File.ReadAllText(filePath);
+            return Deserialize(data);
+        }
+    }
+}
