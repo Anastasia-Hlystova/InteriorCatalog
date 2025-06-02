0c844a925ad2de0b294234d5b321f49b4f048c70
Snezhana1821 <m2405380@edu.misis.ru>
2025-06-03 07:02:22 +1200

    Add files via upload

---
+++ Model/Data/Serialization/JsonSerializer.cs
@@ -0,0 +1,30 @@
+﻿using Model.Core.Serialization;
+using Newtonsoft.Json;
+using System;
+
+namespace Model.Data.Serialization
+{
+    public class JsonSerializer<T> : BaseSerializer<T>
+    {
+        private readonly JsonSerializerSettings _settings;
+
+        public JsonSerializer(JsonSerializerSettings settings = null)
+        {
+            _settings = settings ?? new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto,
+                Formatting = Formatting.Indented
+            };
+        }
+
+        public override string Serialize(T obj)
+        {
+            return JsonConvert.SerializeObject(obj, _settings);
+        }
+
+        public override T Deserialize(string data)
+        {
+            return JsonConvert.DeserializeObject<T>(data, _settings);
+        }
+    }
+}
---
+++ Model/Data/Serialization/XmlSerializer.cs
@@ -0,0 +1,34 @@
+﻿using Model.Core;
+using Model.Core.Serialization;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Model.Data.Serialization
+{
+    public class XmlSerializer<T> : BaseSerializer<T>
+    {
+        private readonly XmlSerializer _serializer;
+        public XmlSerializer()
+        {
+            var extraTypes = new[] { typeof(Chair), typeof(Table), typeof(Sofa), typeof(Bed), typeof(Stool), typeof(Armchair) };
+            _serializer = new XmlSerializer(typeof(T), extraTypes);
+        }
+        public override string Serialize(T obj)
+        {
+            using (var writer = new StringWriter())
+            {
+                _serializer.Serialize(writer, obj);
+                return writer.ToString();
+            }
+        }
+
+        public override T Deserialize(string data)
+        {
+            using (var reader = new StringReader(data))
+            {
+                return (T)_serializer.Deserialize(reader);
+            }
+        }
+    }
+}
